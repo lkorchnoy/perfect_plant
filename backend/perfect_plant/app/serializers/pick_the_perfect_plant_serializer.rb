8206d0f659db7896d9f997135ac1d2b6722dfebe
lkorchnoy <lkorchnoy@yahoo.com>
2020-08-19 18:37:48 -0400

    added myFunction

--- backend/perfect_plant/app/serializers/pick_the_perfect_plant_serializer.rb
+++ backend/perfect_plant/app/serializers/pick_the_perfect_plant_serializer.rb
@@ -1,6 +1,6 @@
 class PickThePerfectPlantSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :question_number, :question, :user #:owner 
+  attributes :question, :user #:owner 
   #def owner 
     #{owner_id: self.object.user.id,
     #owner_name: self.object.user.name}
